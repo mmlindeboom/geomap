1f1d9ca248e15d6a8a11828035e717c50fde39df
Matt Lindeboom <matt.lindeboom@gmail.com>
2013-01-31 18:29:21 -0500

    Add markers
    Button can now add new markers

--- js/map.js
+++ js/map.js
@@ -1,19 +1,19 @@
 /*
  *  Project: Rokkan Bootstrap - Google Map
- *  Description: A plugin to asynchronously insert a Google map or maps into the DOM
+ *  Description: A plugin to insert a Google map or maps into the DOM
  *  Author: Matt Lindeboom
  */
 
 ;(function ( $, window, document, undefined ) {
-
-		//var mapData = {};
 		var map;
-		var markers = {pinpoints: []};
-
+		var markers = {pinpoints: []}; //archive for all pinpoints on the map
+		var newMarkers = {pinpoints: []}; //array to add new pinpoints on map
+		var mapData;
+		var generatedMarker = [];
 		var pluginName = "geoMap",
 				defaults = {
 						mapZoom: undefined,
-						mapData: {},
+						mapData: undefined,
 						geoLocate: true,
 						fitWindow: false
 				};
@@ -23,78 +23,119 @@
 				this.element = element;
 				$element = $(element);
 				canvas = $element.attr('id');
-
 				this.options = $.extend( {}, defaults, options );
+				self = this;
 				this._defaults = defaults;
 				this._name = pluginName;
-				this.geoLocate();
+				this.init(self);
+
+				$('.addMarker').on({
+					click: function() {
+						console.log(self);
+						self.addLocations(self);
+					}
+				});
 		}
 
 		Plugin.prototype = {
-				geoLocate: function() {
-						if (this.options.geoLocate) {
+				init: function(self) {
+						var opts = this.options; //hand off top level options to lower functions
+						if (opts.mapData !== undefined) {
+							self.generateMap(opts);
+						}
+						if (opts.geoLocate) {
 							if(navigator.geolocation) {
 								navigator.geolocation.getCurrentPosition(function(position) {
 										lat = position.coords.latitude;
 										lng = position.coords.longitude;
 										markers = { pinpoints: [{ text: 'Your location', lat: lat, long: lng }] };
-										generateMap();
+										self.generateMap(opts);
 									});
 									} else {
 											handleError();
 									}
 						} else {
-								generateMap();
+								self.generateMap(opts);
 						}
-						var generateMap = function() {
+						
+				},
+				addLocations : function(self, opts) {
+						self.generateMap({
+						mapData: 'data/map-data-set.js'
+					});
+				},
+				
+				generateMap : function(opts) {
+
 							var defaults = {
-								mapsData: {}
+								mapData: opts.mapData
 							};
-							//markers = { pinpoints : [{text:"Philadelphia", lat:39.998012, long: -75.144793},{text: "boston", lat:42.321597, long: -71.089115 }]};
 							var options = $.extend(defaults, options);
-								$.ajax({
-									type: 'get',
-									url: 'data/map-data-set.js',
-									success: function(data){
-										//eval(data);
-										options.mapsData = returnedMapData;
-										addMarkers(options.mapsData);
-									}
-							});
-								function addMarkers(data) {
-									for (j=0; j < options.mapsData.pinpoints.length; j++) {
-										markers.pinpoints.push(options.mapsData.pinpoints[j]);
-									}
-									updateMap(markers);
-								}
+
 							var mapOptions = {
 									zoom:6,
 									center: new google.maps.LatLng(markers.pinpoints[0].lat, markers.pinpoints[0].long),
 									mapTypeId: google.maps.MapTypeId.ROADMAP
 								};
 							map = new google.maps.Map(document.getElementById(canvas), mapOptions);
+							
 							var infowindow = new google.maps.InfoWindow(), marker, i;
 								console.log('The markers are ',markers.pinpoints);
-							
+
+							getMarkers(markers);
+
 							function updateMap(markers) {
-								for (i = 0; i < markers.pinpoints.length; i++) {
-									marker = new google.maps.Marker({
+								console.log('Updating map.');
+								var parseArray;
+								i = generatedMarker.length;
+								console.log(i);
+								for (i ;i < markers.pinpoints.length; i++) {
+									console.log('Inside the loop', i, map);
+									generatedMarker[i] = new google.maps.Marker({
 											position: new google.maps.LatLng(markers.pinpoints[i].lat, markers.pinpoints[i].long),
-											map: map,
-											animation: google.maps.Animation.DROP
+											map: map
+											//animation: google.maps.Animation.DROP
 									});
-									google.maps.event.addListener(marker, 'click', (function(marker, i) {
+									newMarker = generatedMarker[i];
+									google.maps.event.addListener(generatedMarker[i], 'click', (function(generatedMarker, i) {
 												return function() {
 														infowindow.setContent(markers.pinpoints[i].text);
-														infowindow.open(map, marker);
+														infowindow.open(map, generatedMarker);
 												};
-										})(marker, i));
+										})(generatedMarker[i], i));
 								}
 							}
-							
-						};
-				},
+							function getMarkers(markers) {
+								if (options.mapData !== undefined) {
+									$.ajax({
+										type: 'get',
+										url: options.mapData,
+										success: function(data){
+											//eval(data);
+											options.mapsData = returnedMapData;
+											addMarkers(options.mapsData);
+										}
+									});
+								} else {
+									console.log('We got to the else condition');
+									updateMap(markers);
+								}
+							}
+							function addMarkers(data) {
+								for (j=0; j < options.mapsData.pinpoints.length; j++) {
+									newMarkers.pinpoints.push(options.mapsData.pinpoints[j]);
+								} 
+								updateMarkers(newMarkers);
+							}
+							function updateMarkers(newMarkers) {
+								for (k=0; k < newMarkers.pinpoints.length; k++) {
+										markers.pinpoints.push(newMarkers.pinpoints[k]);
+								}
+								updateMap(markers);
+							}
+						},
 
+				
 				fitWindow: function() {
 						console.log('Fitting');
 						$element.css({width: $(window).width(), height: $(window).height(), position: 'absolute', 'z-index': 1, top:0, left:0, margin:0});
--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 
 (function($){
 	//Initiate Map
-	$('#map_canvas').geoMap({
-		mapsData: 'data/map-data-set.js'
+	$('#map_canvas').geoMap();
+
+	$('.addMarker').click(function(){
+		console.log('addMarker has been clicked');
+		$('#map_canvas').geoMap({
+			mapData : 'data/map-data-set.js'
 		});
+	});
 }(jQuery));
